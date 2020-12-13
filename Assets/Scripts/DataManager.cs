1717d126dc3a20e1f5beb66416de5f6321832774
Iker <ikernoya4@gmail.com>
2020-12-13 13:25:22 -0300

    Created Unity Project

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        if (instance != null)
+             instance = null;
+    }
+    private void OnDestroy()
+    {
+        if (instance != null)
+            instance = null;
+    }
+}
