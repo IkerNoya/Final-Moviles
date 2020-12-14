e5dc5d5dbe2d78e57407f72cc4014728b0210671
Iker <ikernoya4@gmail.com>
2020-12-13 22:31:13 -0300

    Created enemy spawners

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 direction = Vector3.left;
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
